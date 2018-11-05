ceba4a9408dabb3b65bc2695c5fa1cab1fa9ab14
jarfar <krabiksinij@gmail.com>
2018-11-05 23:46:37 +0200

    fix

--- packages/dmgmod/index.js
+++ packages/dmgmod/index.js
@@ -1,6 +1,11 @@
 function dmG (player, healthLoss, armorLoss) {
     const pd = player.name;
+    const hl = healthLoss;
+    const al = armorLoss;
     mp.players.broadcast(`${pd}`);
+    mp.players.broadcast(`${hl}`);
+    mp.players.broadcast(`${al}`);
+    mp.players.broadcast(ok);
 }
 
 mp.events.add("playerDamage", dmG);
