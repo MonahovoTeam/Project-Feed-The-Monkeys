4aeac233f09487ee71294de7b033419d249edbb3
jarfar <krabiksinij@gmail.com>
2018-11-05 22:37:47 +0200

    del fingerpoint

--- client_packages/dmgmod/index.js
+++ client_packages/dmgmod/index.js
@@ -1,5 +1,6 @@
-function playerSpawn(player) {
+function playerSpawnHandler(player) {
+    mp.players.broadcast(player.name);
     player.setWeaponDamageModifier(10000);
 }
 
-mp.events.add("playerSpawn", playerSpawn);
+mp.events.add("playerSpawn", playerSpawnHandler);
