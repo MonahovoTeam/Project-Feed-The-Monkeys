831cf9d045235d9b3c5c47999152fa7aa0970185
jarfar <krabiksinij@gmail.com>
2018-11-06 00:57:50 +0200

    fix

--- client_packages/dmgmod/index.js
+++ client_packages/dmgmod/index.js
@@ -1,3 +1,9 @@
-mp.events.add('playerWeaponShot', (targetPosition, targetEntity) => {
+// mp.events.add('playerWeaponShot', (targetPosition, targetEntity) => {
+//     mp.gui.chat.push('You fired a weapon!');
+// });
+
+function playerSpawn(player) {
     mp.gui.chat.push('You fired a weapon!');
-});
+}
+
+mp.events.add("playerSpawn", playerSpawn);
