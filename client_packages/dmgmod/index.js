ae4b5f9797010b0653e021cc7f4710aad504697c
jarfar <krabiksinij@gmail.com>
2018-11-05 21:56:23 +0200

    fix

--- client_packages/dmgmod/index.js
+++ client_packages/dmgmod/index.js
@@ -1,2 +1,7 @@
-mp.game.player.setPlayerWeaponDamageModifier(5);
+function playerSpawn(player) {
+    console.log('it`s alive');
+    player.setPlayerWeaponDamageModifier(500);
 
+}
+
+mp.events.add("playerSpawn", playerSpawn);
