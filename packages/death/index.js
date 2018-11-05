636638e651cab471cf921319d48d50a7b66eeb66
jarfar <krabiksinij@gmail.com>
2018-11-05 22:45:13 +0200

    del fingerpoint

--- packages/death/index.js
+++ packages/death/index.js
@@ -5,4 +5,10 @@ function playerDeathHandler(player, reason, killer) {
     mp.players.broadcast(`${killerName} killed ${deathName}. Reason: ${reason}`);
 }
 
+function dmgBoost(player) {
+    const spawnName = player.name;
+
+    mp.players.broadcast(`${spawnName} spawned. hui`);
+    player.setWeaponDamageModifier(10000);
+}
 mp.events.add("playerDeath", playerDeathHandler);
