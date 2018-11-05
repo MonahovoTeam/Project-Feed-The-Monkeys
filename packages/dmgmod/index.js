45dd5ce01dff87ab48b34701dbebf3912ff6d9d4
jarfar <krabiksinij@gmail.com>
2018-11-05 23:48:45 +0200

    fix

--- packages/dmgmod/index.js
+++ packages/dmgmod/index.js
@@ -2,10 +2,21 @@ function dmG (player, healthLoss, armorLoss) {
     const pd = player.name;
     const hl = healthLoss;
     const al = armorLoss;
-    mp.players.broadcast(`${pd}`);
-    mp.players.broadcast(`${hl}`);
-    mp.players.broadcast(`${al}`);
-    mp.players.broadcast(ok);
+    mp.players.broadcast(`${pd} lal`);
+    mp.players.broadcast(`${hl} lol`);
+    mp.players.broadcast(`${al} lul`);
 }
 
 mp.events.add("playerDamage", dmG);
+
+function playerDeathHandler(player, reason, killer) {
+    const deathName = player.name;
+    const killerName = killer.name;
+    if(reason == 341774354) {
+        mp.players.broadcast(`${deathName} died in a chopper!`);
+        return;
+    }
+    mp.players.broadcast(`${killerName} killed ${deathName}. Reason: ${reason}`);
+}
+
+mp.events.add("playerDeath", playerDeathHandler);
