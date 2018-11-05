6e4e955124a0b96270928e42a4a5c7b7e793090c
Logimy <ar.vdow@yandex.ru>
2018-11-06 00:33:05 +0200

    Попытка в увеличенный дамаг

--- client_packages/dmgmod/index.js
+++ client_packages/dmgmod/index.js
@@ -1,6 +1,11 @@
-function playerSpawn(player) {
-    console.log(`${player.name} has spawned`);
-    player.setWeaponDamageModifier(1000000);
-}
+// function playerSpawn(player) {
+//    console.log(`${player.name} has spawned`);
+//    player.setWeaponDamageModifier(1000000);
+// }
 
-mp.events.add("playerSpawn", playerSpawn);
+// mp.events.add("playerSpawn", playerSpawn);
+
+
+mp.events.add('playerJoinedServer', (id, name) => {
+        player.setWeaponDamageModifier(1000000);
+});
