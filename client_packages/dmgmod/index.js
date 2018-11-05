ef9d3ff071e2df5a47cd5c8519b6ac3ceddd8217
jarfar <krabiksinij@gmail.com>
2018-11-06 01:10:06 +0200

    fix

--- client_packages/dmgmod/index.js
+++ client_packages/dmgmod/index.js
@@ -1,4 +1,4 @@
-mp.events.add('playerJoin', player => {
+mp.events.add('playerSpawn', player => {
     mp.gui.chat.push(`[SERVER]: ${player.name} has joined the server!`);
     mp.game.player.setPlayerWeaponDamageModifier(250);
 });
